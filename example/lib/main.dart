9e652d2436d4cd531109adce1ffc43115b741c5a
deuque <dchiaha@gmail.com>
2023-04-29 15:54:52 +0100

    update project example overlay color

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -68,7 +68,7 @@ class _MySamplePageState extends State<MySamplePage> {
         child: Container(
           height: double.infinity,
           width: double.infinity,
-          color: Colors.yellow.withOpacity(.2),
+          color: Colors.blue.withOpacity(.2),
         ),
       ),
       builder: (context) => Scaffold(
